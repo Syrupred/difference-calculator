7f8f7b94d55e72c2147be240815628270b616278
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-12-18 14:40:52 +0300

    change gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,5 +3,7 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
   .version('1.0.0', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option( '-f, --format [type]' ,  'output format' );
 program.parse();
