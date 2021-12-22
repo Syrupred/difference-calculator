557f0446b7878963a3c2e8f6b92dc9a3c3fd189c
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-12-22 17:13:00 +0300

    change options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,11 +5,11 @@ import genDiff from '../src/diff.js';
 
 const program = new Command();
 program
-  .version('1.0.0', '-V, --version', 'output the version number')
+  .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
-  })
-  .option('-f, --format [type]', 'output format');
-program.parse();
+  });
+program.parse(process.argv);
