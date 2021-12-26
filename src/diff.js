41ab7088a699750c1de11d51f7b74b0e5b57fefe
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-12-26 17:58:02 +0300

    delete getDiffFiles, add format

--- src/diff.js
+++ src/diff.js
@@ -1,36 +1,17 @@
 import { readFileSync } from 'fs';
-import _ from 'lodash';
 import path from 'path';
 import parseData from './parsers.js';
+import buildTree from './buildTree.js';
+import makeStylishFormat from './formatters/stylish.js';
 
-const getDiffFiles = (file1, file2, sign = ' ', number = 4) => {
-  const dataKeys = _.sortBy(Object.keys({ ...file1, ...file2 }));
-  const indent = sign.repeat(number);
-  const iter = (data1, data2) => {
-    const comparison = dataKeys.reduce((acc, key) => {
-      if (!data2[key]) {
-        return `${acc}${indent}- ${key}: ${data1[key]}\n`;
-      }
-      if (!data1[key]) {
-        return `${acc}${indent}+ ${key}: ${data2[key]}\n`;
-      }
-      if (data1[key] === data2[key]) {
-        return `${acc}${indent}  ${key}: ${data1[key]}\n`;
-      }
-      if (data1[key] !== data2[key]) {
-        return `${acc}${indent}- ${key}: ${data1[key]}\n${indent}+ ${key}: ${data2[key]}\n`;
-      }
-      return acc;
-    }, '');
-    return comparison;
-  };
-  return `{\n${iter(file1, file2)}  }`;
-};
-
-export default (path1, path2) => {
+export default (path1, path2, format = 'stylish') => {
   const extPath1 = path.parse(path1).ext;
   const extPath2 = path.parse(path2).ext;
   const file1 = parseData((readFileSync(path.resolve(process.cwd(), path1), 'utf-8')), extPath1);
   const file2 = parseData((readFileSync(path.resolve(process.cwd(), path2), 'utf-8')), extPath2);
-  return getDiffFiles(file1, file2);
+  const comparison = buildTree(file1, file2);
+  if (format === 'stylish') {
+    return makeStylishFormat(comparison);
+  }
+  return makeStylishFormat(comparison);
 };
