68de74814925cd71c704160b1f19f46d701fba7b
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-12-27 01:13:34 +0300

    add options.format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
+import program from 'commander';
+import genDiff from '../index.js';
 
-import { Command } from 'commander/esm.mjs';
-import genDiff from '../src/diff.js';
-
-const program = new Command();
 program
-  .version('0.0.1', '-V, --version', 'output the version number')
+  .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const { format } = program;
-    console.log(genDiff(filepath1, filepath2, format));
+    const options = program.opts();
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
   });
 program.parse(process.argv);
