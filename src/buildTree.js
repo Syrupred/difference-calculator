0d1118930e874e6ce81dfa252935691b6c8987ed
Ирина Харитонова <kuvirinaaa@gmail.com>
2021-12-26 17:50:58 +0300

    add buildTree

---
+++ src/buildTree.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+const buildTree = (data1, data2) => {
+  const dataKeys = _.sortBy(Object.keys({ ...data1, ...data2 }));
+  return dataKeys.map((key) => {
+    const valueData1 = data1[key];
+    const valueData2 = data2[key];
+
+    if (!_.has(data1, key)) {
+      return { name: key, status: 'added', value: valueData2 };
+    }
+    if (!_.has(data2, key)) {
+      return { name: key, status: 'deleted', value: valueData1 };
+    }
+    if (_.isObject(valueData1) && _.isObject(valueData2)) {
+      return {
+        name: key, status: 'hasChildren', children: buildTree(valueData1, valueData2),
+      };
+    }
+    if (valueData1 !== valueData2) {
+      return {
+        name: key, status: 'changed', oldValue: valueData1, newValue: valueData2,
+      };
+    }
+    return { name: key, status: 'unchanged', value: valueData1 };
+  });
+};
+export default buildTree;
